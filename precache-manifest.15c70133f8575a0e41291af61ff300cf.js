f0875409101c0aa1b0d2ae8be8699b96339ae56e
tayta21 <ahmedtritar21@gmail.com>
2020-08-05 17:12:14 +0200

    Updates

---
+++ precache-manifest.15c70133f8575a0e41291af61ff300cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd324f77b02380fd771564b979bd2db",
+    "url": "/state/index.html"
+  },
+  {
+    "revision": "3d8b948dc4186c50f222",
+    "url": "/state/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34b0ffae77b9a4824891",
+    "url": "/state/static/js/2.21344847.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/state/static/js/2.21344847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8b948dc4186c50f222",
+    "url": "/state/static/js/main.e98e6f33.chunk.js"
+  },
+  {
+    "revision": "8079d184367af7c794fa",
+    "url": "/state/static/js/runtime-main.890111fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/state/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d8ea3e525780ba0b8572575e5bc631a",
+    "url": "/state/static/media/rick.4d8ea3e5.png"
+  }
+]);
