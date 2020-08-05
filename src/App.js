d4fcf576e7dd52873007dfab7490bb1f30ee1a7c
tayta21 <ahmedtritar21@gmail.com>
2020-08-05 17:10:47 +0200

    react state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firstpic from "./img/rick.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state= {
+    person : {
+      fullName:"rick",
+      imgSrc : firstpic,
+      profession : "scientiste"
+    },
+    verify : false,
+    time : 0
+  }
+ 
+  }
+  
+  handleShow = () => {
+    this.setState({verify:true})
+  }
+ handleshowtwo =() =>{
+  this.setState({verify:false})
+ }
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({time : this.state.time +1})
+    }, 1000);
+  }
+
+  render() {
+    return <div>
+      {this.state.verify ?<div>
+        <h1>{this.state.person.fullName}</h1>
+        <img src={this.state.person.imgSrc}/>
+        <h1>{this.state.person.profession}</h1>
+        <button onClick={this.handleshowtwo}>return</button>
+      </div> 
+      : 
+      <button onClick={this.handleShow}>Allow</button> }
+      <h1>{this.state.time}</h1>       
+      </div>
+  }
 }
-
 export default App;
